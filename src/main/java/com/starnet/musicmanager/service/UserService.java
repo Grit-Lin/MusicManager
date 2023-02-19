186782e896ecaf5b4918ff621167d143ad563229
rsooio <424416785@qq.com>
2023-02-20 02:27:09 +0800

    Feat: update UserService

--- src/main/java/com/starnet/musicmanager/service/UserService.java
+++ src/main/java/com/starnet/musicmanager/service/UserService.java
@@ -1,13 +1,9 @@
 package com.starnet.musicmanager.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
-import com.starnet.musicmanager.common.R;
 import com.starnet.musicmanager.entity.User;
-import com.starnet.musicmanager.mapper.UserMapper;
-import com.sun.org.apache.xpath.internal.operations.Bool;
-import org.springframework.beans.factory.annotation.Autowired;
 
 public interface UserService extends IService<User> {
 
-    public Boolean login(String username, String password);
+    User login(String username, String password);
 }
