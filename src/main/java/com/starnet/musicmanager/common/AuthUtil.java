48445d908c5862c91a16ea05adcdc4da09ec6801
rsooio <424416785@qq.com>
2023-02-20 02:32:16 +0800

    Feat: complete auth logic

--- src/main/java/com/starnet/musicmanager/common/AuthUtil.java
+++ src/main/java/com/starnet/musicmanager/common/AuthUtil.java
@@ -1,11 +1,15 @@
 package com.starnet.musicmanager.common;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 public class AuthUtil {
+    private static final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
     static public String Encrypt(String password) {
-        return password;
+        return encoder.encode(password);
     }
 
     static public Boolean Verify(String password, String hashedPassword) {
-        return password.equals(hashedPassword);
+        return encoder.matches(password, hashedPassword);
     }
 }
