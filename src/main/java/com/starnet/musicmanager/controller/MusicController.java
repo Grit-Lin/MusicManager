a37cd06e6de9d4755eb65a478ddd6eb7e9773633
rsooio <424416785@qq.com>
2023-02-20 08:51:16 +0800

    Feat: add music list simulator

--- src/main/java/com/starnet/musicmanager/controller/MusicController.java
+++ src/main/java/com/starnet/musicmanager/controller/MusicController.java
@@ -1,13 +1,12 @@
 package com.starnet.musicmanager.controller;
 
-import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.starnet.musicmanager.common.R;
-import com.starnet.musicmanager.entity.Music;
-import com.starnet.musicmanager.service.serviceImpl.MusicServiceImpl;
+import com.starnet.musicmanager.dto.MusicDTO;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -18,14 +17,20 @@ import java.util.List;
 @RestController
 @RequestMapping("/music")
 public class MusicController {
-
-    private MusicServiceImpl musicService;
-
     @GetMapping("/list")
-    public R<List<Music>> list() {
-        LambdaQueryWrapper<Music> lqw = new LambdaQueryWrapper<>();
-        lqw.orderByAsc(Music :: getId);
-        List<Music> list = musicService.list(lqw);
+    public R<List<MusicDTO>> list() {
+//        LambdaQueryWrapper<Music> lqw = new LambdaQueryWrapper<>();
+//        lqw.orderByAsc(Music :: getId);
+//        List<Music> list = musicService.list(lqw);
+        List<MusicDTO> list = new ArrayList<>();
+        for (long i = 0L; i < 30; i++) {
+            MusicDTO music = new MusicDTO();
+            music.setId(i);
+            music.setName("name" + i);
+            music.setSinger("singer" + i);
+            music.setAlbum("album" + i);
+            list.add(music);
+        }
         return R.success(list);
     }
 
